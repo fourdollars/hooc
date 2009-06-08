03357ac408ff08cf0c5dfd305ef81d8a2621d579
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2009-06-08 18:57:18 +0800

    Let MyPlayer implement AbstractPlayer.

--- MyPlayer.h
+++ MyPlayer.h
@@ -1,13 +1,15 @@
 #include <features.h>
 #include <stdbool.h>
+#include "AbstractPlayer.h"
 #ifndef __MY_PLAYER_H__
 #define __MY_PLAYER_H__
 __BEGIN_DECLS
 
 /**
  * @class MyPlayer
+ * @implements AbstractPlayer
  */
-typedef struct MyPlayer MyPlayer;
+typedef struct AbstractPlayer MyPlayer;
 
 /**
  * @brief Use this to create object of MyPlayer.
@@ -16,28 +18,6 @@ typedef struct MyPlayer MyPlayer;
  * @retval NULL If it fails.
  */
 MyPlayer* MyPlayer_create(void);
-
-struct MyPlayer {
-    /** @cond */
-    void* _private;
-    /** @endcond */
-
-    /** @cond */
-    bool (*open)(MyPlayer* self, const char* url);
-    /** @endcond */
-
-    /** @cond */
-    bool (*play)(MyPlayer* self);
-    /** @endcond */
-
-    /** @cond */
-    bool (*close)(MyPlayer* self);
-    /** @endcond */
-
-    /** @cond */
-    bool (*destroy)(MyPlayer* self);
-    /** @endcond */
-};
 
 __END_DECLS
 #endif //__MY_PLAYER_H__
